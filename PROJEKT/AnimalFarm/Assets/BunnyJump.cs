8b5542f55dfe80018cd097836af62a281de6b075
Vera Malieske <Vmaliesk@smail.uni-koeln.de>
2018-05-26 13:26:22 +0200

    Dies und Das
    BunnyTest

--- PROJEKT/AnimalFarm/Assets/BunnyJump.cs
+++ PROJEKT/AnimalFarm/Assets/BunnyJump.cs
@@ -15,12 +15,22 @@ public class BunnyJump : MonoBehaviour
     void Start()
     {
         Player = GameObject.Find("Player");
+        speech();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 newPos = (this.transform.position - Player.transform.position).normalized + Player.transform.position;
-        this.transform.position = new Vector3(newPos.x, newPos.y, newPos.z);
+        this.transform.position = new Vector3(newPos.x + 1, newPos.y, newPos.z);
+
+    }
+
+    void speech()
+    {
+        if (!SceneTrigger.boxerGone)
+        {
+            print("Look, Boxer's sick");
+        }
     }
 }
