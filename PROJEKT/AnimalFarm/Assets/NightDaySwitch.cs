e5815c9b25ab9a4afa9af450545f2f089c0286ed
Vera Malieske <Vmaliesk@smail.uni-koeln.de>
2018-06-07 12:31:16 +0200

    Tag/Nacht Switch
    Grade noch mit Tastendruck (F)

---
+++ PROJEKT/AnimalFarm/Assets/NightDaySwitch.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightDaySwitch : MonoBehaviour {
+
+    public bool lightOn;
+    public Light sun;
+    public Material skyboxDay;
+    public Material skyboxNight;
+
+    public Color fullLight;
+    public Color fullDark;
+
+    // Use this for initialization
+    void Start()
+    {
+        sun.enabled = true;
+        RenderSettings.skybox = skyboxDay;
+        lightOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+
+            if (lightOn == false)
+            {
+                sun.enabled = false;
+                RenderSettings.skybox = skyboxNight;
+                RenderSettings.ambientLight = fullDark;
+                lightOn = true;
+            }
+            else
+            {
+                sun.enabled = true;
+                RenderSettings.skybox = skyboxDay;
+                RenderSettings.ambientLight = fullLight;
+                lightOn = false;
+            }
+        }
+    }
+}
